22b062822c5f5b12d3a5b08e6fb973df3863bff7
Jacob Hummer <jcbhmr@outlook.com>
2024-05-08 19:06:50 +0000

    works on linux!

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let dest = xdg_dirs.get_data_home();
 
     let exe_ext = if cfg!(windows) { ".exe" } else { "" };
-    let status = std::process::Command::new(dest.join("bikeshed".to_owned() + exe_ext)).status()?;
-    if status.success() {
-        Ok(())
-    } else {
-        Err("not success".into())
+    let mut cmd = std::process::Command::new(dest.join("bikeshed".to_owned() + exe_ext));
+    let args = std::env::args_os().into_iter().collect::<Vec<_>>();
+    cmd.args(&args[1..]);
+    #[cfg(unix)]
+    {
+        use std::os::unix::process::CommandExt;
+        cmd.arg0(&args[0]);
+        let error = cmd.exec();
+        return Err(Box::new(error));
     }
+    #[cfg(not(unix))]
+    cmd;
+    return Ok(());
 }
